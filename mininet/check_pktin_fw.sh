90a1763139d3e49f2d27f2353022e22b5200be68
David Ke Hong <hongkedavid@gmail.com>
2015-05-09 18:33:42 -0400

    Update check_pktin_fw.sh

--- mininet/check_pktin_fw.sh
+++ mininet/check_pktin_fw.sh
@@ -5,33 +5,35 @@ tcpdump -nnvvXSs 1514 'dst 141.212.108.10 and dst port 6653 and tcp[20:2] = 0x01
 i=1000;
 while [ $i -le 10000 ];
 do
-     a=$(cat out.out | grep "tp_dst=$i" | wc -l);
+     a=$(cat $2 | grep "tp_dst=$i" | wc -l);
      h=$(printf "%04x\n" $i);
-     b=$(cat tmp | grep " $h " | grep  "0a00 002a\| 05b0 " | wc -l);
-     if [ $b -eq 1 ]; then
-         t=$(cat tmp | grep " $h " | grep  "0a00 002a\| 05b0 " | grep -b -o "$h" | cut -d':' -f1);
-         t1=0;
-         t2=0;
-         if [ $(cat tmp | grep " $h " | grep  "0a00 002a\| 05b0 " | grep -b -o "0a00 002a" | wc -l) -gt 0 ]; then
-             t1=$(cat tmp | grep " $h " | grep  "0a00 002a\| 05b0 " | grep -b -o "0a00 002a" | cut -d':' -f1);
-             if [ $(($t-$t1)) -ne 15 ]; then
-                 b=0;
+     b=$(cat $1 | grep " $h " | grep  "0a00 002a\| 05b0 " | wc -l);
+     j=0;
+     cnt=0;
+     while [ $j -le $b ]; do
+         t=$(cat tmp | grep " $h " | grep  "0a00 002a\| 05b0 " | head -n$j | tail -n1 | grep -b -o "$h" | cut -d':' -f1);
+         if [ $(cat tmp | grep " $h " | grep  "0a00 002a\| 05b0 " | head -n$j | tail -n1 | grep -b -o "0a00 002a" | wc -l) -gt 0 ]; then
+             t1=$(cat tmp | grep " $h " | grep  "0a00 002a\| 05b0 " | head -n$j | tail -n1 | grep -b -o "0a00 002a" | cut -d':' -f1);
+             if [ $(($t-$t1)) -eq 15 ]; then
+                 cnt=$(($cnt+1));
              fi
          fi
-         if [ $(cat tmp | grep " $h " | grep  "0a00 002a\| 05b0 " | grep -b -o " 05b0 " | wc -l) -gt 0 ]; then
-             t2=$(cat tmp | grep " $h " | grep  "0a00 002a\| 05b0 " | grep -b -o "05b0 " | cut -d':' -f1);
-             if [ $(($t2-$t)) -ne 5 ]; then
-                 b=0;
+         if [ $(cat tmp | grep " $h " | grep  "0a00 002a\| 05b0 " | head -n$j | tail -n1 | grep -b -o " 05b0 " | wc -l) -gt 0 ]; then
+             t2=$(cat tmp | grep " $h " | grep  "0a00 002a\| 05b0 " | head -n$j | tail -n1 | grep -b -o "05b0 " | cut -d':' -f1);
+             if [ $(($t2-$t)) -eq 5 ]; then
+                 cnt=$(($cnt+1));
              fi
          fi
-     fi
+         j=$(($J+1));
+     done
+     b=$cnt;
      if [ $(($a+$b)) -eq 0 ]; then
         i=$(($i+1));
         continue;
      fi
      c=$(($a*$b));
      if [ $c -eq 0 ]; then
-        echo "$i: $a $b";
+        echo "$i: $a $b $cnt";
      fi
      i=$(($i+1));
 done
